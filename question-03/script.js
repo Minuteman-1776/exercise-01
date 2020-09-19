56ab555440c281092a0136ad1b6bac7da1b0d669
Alfatah Wibisono <68675356+Minuteman-1776@users.noreply.github.com>
2020-09-19 13:39:07 +0700

    update

--- question-03/script.js
+++ question-03/script.js
@@ -5,25 +5,28 @@ var finalPayments = [];
 
 var billTipCalculator = function(number){
     if(number < 50){
-        tips.push(number)
-        console.log('Bill is less than 50, so the tip will be 20% of the bill')
-        return bills;
+        var total;
+        total = number*25/100;
+		tips.push(number)
+      	finalPayments.push(total)
+        return total;
     }else if(number == 50 || number < 201){
-        tips.push(number)
-        console.log('Bill is equal to fifty and is less than 201, so the tip will be 15% of the bill')
-        return bills;
+        var total 
+        total = number*15/100;
+ 		tips.push(number)
+      	finalPayments.push(total)
+        return total;
     }else if(number > 200){
-        tips.push(number)
-        console.log('Bill is more than 200, so the tip will be 10% of the bill.')
-        return bills;
+        var total 
+        total = number*10/100;         
+		tips.push(number)
+      	finalPayments.push(total)
+        return total;
     }
 }
-billTipCalculator(40)
-billTipCalculator(80)
-billTipCalculator(225)
-
-
-//gua ngerjain apa adanya ko wkwkw
+billTipCalculator(124)
+billTipCalculator(48)
+billTipCalculator(248)
 
 
 console.log(tips)
